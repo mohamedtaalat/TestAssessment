0331624b4744f172f0afed4047ccfe77ba088a66
Mohamed Talat <mohamedabotalat57@gmail.com>
2026-07-15 12:49:00 +0300

    Add data for test login and test for test login

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,12 +1,27 @@
 import json
+import logging
+import time
 
 import pytest
+
+from pages.header import Header
+from pages.login_page import LoginPage
 
 
 @pytest.mark.usefixtures("driver")
 class TestLogin:
-    with open("data/super_admin/articles/happy_scenarios.json") as f:
+    with open("data/login_data.json") as f:
         data = json.load(f)
     @pytest.mark.parametrize("data", data)
     def test_login(self,driver,data):
-        pass
+        hdp = Header(driver)
+        hdp.click_login()
+        lgp = LoginPage(driver)
+        lgp.login(data["email"], data["password"])
+        time.sleep(2)
+        print(data["Scenario"])
+        print(f"Expected : {data['expected']}")
+        if data["expected"] == "Login" :
+            assert driver.current_url != "https://asciisd-software-testing.on-forge.com/login"
+        elif data["expected"] == "Not Login" :
+            assert driver.current_url == "https://asciisd-software-testing.on-forge.com/login"
